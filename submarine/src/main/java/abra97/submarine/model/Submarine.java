37c1d89e5e7fc033d8725cf4b842417748f8140a
Alex Toth <tothalex95@gmail.com>
2016-11-04 10:35:11 +0100

    submarine

--- submarine/src/main/java/abra97/submarine/model/Submarine.java
+++ submarine/src/main/java/abra97/submarine/model/Submarine.java
@@ -1,13 +1,26 @@
 package abra97.submarine.model;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class Submarine {
 
 	private int id;
 	private Position position;
 	private Team owner;
 	
+	public static int COUNT_PER_TEAM;
 	public static int MAX_STEERING_PER_ROUND;
 	public static int MAX_ACCELERATION_PER_ROUND;
 	public static int MAX_SPEED;
+	
+	public static void initialize(String json) {
+		JSONTokener tokener = new JSONTokener(json);
+		JSONObject root = new JSONObject(tokener).getJSONObject("game").getJSONObject("mapConfiguration");
+		COUNT_PER_TEAM = root.getInt("submarinesPerTeam");
+		MAX_ACCELERATION_PER_ROUND = root.getInt("maxAccelerationPerRound");
+		MAX_SPEED = root.getInt("maxSpeed");
+		MAX_STEERING_PER_ROUND = root.getInt("maxSteeringPerRound");
+	}
 	
 }
