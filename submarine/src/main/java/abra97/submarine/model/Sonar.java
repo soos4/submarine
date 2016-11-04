f8be89148e9e990eb55e51cadd41bf4eb20fcae4
horvath23 <horvath23@iit323.iit.uni-miskolc.hu>
2016-11-04 10:31:02 +0100

    Sonar static vars + parser

--- submarine/src/main/java/abra97/submarine/model/Sonar.java
+++ submarine/src/main/java/abra97/submarine/model/Sonar.java
@@ -1,8 +1,22 @@
 package abra97.submarine.model;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class Sonar {
 
-	
-	
+	public static int SONAR_RANGE;
+	public static int EXTENDED_SONAR_RANGE;
+    public static int EXTENDED_SONAR_ROUNDS;
+    public static int EXTENDED_SONAR_COOLDOWN;
+
+    public static void initialize(String json) {
+		JSONTokener tokener = new JSONTokener(json);
+		JSONObject root = new JSONObject(tokener).getJSONObject("game").getJSONObject("mapConfiguration");
+		SONAR_RANGE = root.getInt("sonarRange");
+		EXTENDED_SONAR_RANGE = root.getInt("extendedSonarRange");
+		EXTENDED_SONAR_ROUNDS = root.getInt("extendedSonarRounds");
+		EXTENDED_SONAR_COOLDOWN = root.getInt("extendedSonarCooldown");
+	}
 	
 }
