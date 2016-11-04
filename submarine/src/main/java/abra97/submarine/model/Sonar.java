446921faec4cbaddf19571c7975a7a04096d4069
Alex Toth <tothalex95@gmail.com>
2016-11-04 14:02:40 +0100

    sonar

--- submarine/src/main/java/abra97/submarine/model/Sonar.java
+++ submarine/src/main/java/abra97/submarine/model/Sonar.java
@@ -7,15 +7,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-
 public class Sonar {
 
 	public static int SONAR_RANGE;
 	public static int EXTENDED_SONAR_RANGE;
-    public static int EXTENDED_SONAR_ROUNDS;
-    public static int EXTENDED_SONAR_COOLDOWN;
+	public static int EXTENDED_SONAR_ROUNDS;
+	public static int EXTENDED_SONAR_COOLDOWN;
 
-    public static void initialize(String json) {
+	public static void initialize(String json) {
 		JSONTokener tokener = new JSONTokener(json);
 		JSONObject root = new JSONObject(tokener).getJSONObject("game").getJSONObject("mapConfiguration");
 		SONAR_RANGE = root.getInt("sonarRange");
@@ -23,23 +22,30 @@ public class Sonar {
 		EXTENDED_SONAR_ROUNDS = root.getInt("extendedSonarRounds");
 		EXTENDED_SONAR_COOLDOWN = root.getInt("extendedSonarCooldown");
 	}
-    
-    public Collection<Entity> getEntities(String json) {
-    	JSONTokener tokener = new JSONTokener(json);
+
+	public Collection<Entity> getEntities(String json) {
+		JSONTokener tokener = new JSONTokener(json);
 		JSONObject root = new JSONObject(tokener);
 		JSONArray arr = root.getJSONArray("entities");
 		Collection<Entity> entities = new ArrayList<>();
 		for (int i = 0; i < arr.length(); i++) {
 			JSONObject ent = arr.getJSONObject(i);
 			if (ent.getString("type").equals("Submarine")) {
-				Entity sub = new Submarine(ent.getInt("id"), new Position(ent.getJSONObject("position").getDouble("x"), ent.getJSONObject("position").getDouble("y")), new Team(ent.getJSONObject("owner").getString("name")), ent.getDouble("velocity"), new Direction(ent.getDouble("angle")));
+				Entity sub = new Submarine(ent.getInt("id"),
+						new Position(ent.getJSONObject("position").getDouble("x"),
+								ent.getJSONObject("position").getDouble("y")),
+						new Team(ent.getJSONObject("owner").getString("name")), ent.getDouble("velocity"),
+						new Direction(ent.getDouble("angle")));
 				entities.add(sub);
-			}
-			if (ent.getString("type").equals("Torpedo")) {
-				Entity torpedo = new Torpedo(ent.getInt("id"), new Position(ent.getJSONObject("position").getDouble("x"), ent.getJSONObject("position").getDouble("y")), new Team(ent.getJSONObject("owner").getString("name")), ent.getDouble("velocity"), new Direction(ent.getDouble("angle")));
+			} else if (ent.getString("type").equals("Torpedo")) {
+				Entity torpedo = new Torpedo(ent.getInt("id"),
+						new Position(ent.getJSONObject("position").getDouble("x"),
+								ent.getJSONObject("position").getDouble("y")),
+						new Team(ent.getJSONObject("owner").getString("name")), ent.getDouble("velocity"),
+						new Direction(ent.getDouble("angle")));
 				entities.add(torpedo);
 			}
 		}
 		return entities;
-    }
+	}
 }
