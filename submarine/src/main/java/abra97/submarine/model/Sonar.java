193963febb5d640d467dc5b37e382d3dbfc95f21
horvath23 <horvath23@iit323.iit.uni-miskolc.hu>
2016-11-04 12:39:27 +0100

    Sonar getentities

--- submarine/src/main/java/abra97/submarine/model/Sonar.java
+++ submarine/src/main/java/abra97/submarine/model/Sonar.java
@@ -1,7 +1,13 @@
 package abra97.submarine.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.w3c.dom.Entity;
 
 public class Sonar {
 
@@ -18,4 +24,23 @@ public class Sonar {
 		EXTENDED_SONAR_ROUNDS = root.getInt("extendedSonarRounds");
 		EXTENDED_SONAR_COOLDOWN = root.getInt("extendedSonarCooldown");
 	}
+    
+    public Collection<Entity> getEntities(String json) {
+    	JSONTokener tokener = new JSONTokener(json);
+		JSONObject root = new JSONObject(tokener);
+		JSONArray arr = root.getJSONArray("entities");
+		Collection<Entity> entities = new ArrayList<>();
+		for (int i = 0; i < arr.length(); i++) {
+			JSONObject ent = arr.getJSONObject(i);
+			if (ent.getString("type").equals("Submarine")) {
+				Entity sub = (Entity) new Submarine(ent.getInt("id"), new Position(ent.getJSONObject("position").getDouble("x"), ent.getJSONObject("position").getDouble("y")), new Team(ent.getJSONObject("owner").getString("name")), ent.getDouble("velocity"), new Direction(ent.getDouble("angle")));
+				entities.add(sub);
+			}
+			if (ent.getString("type").equals("Torpedo")) {
+				Entity torpedo = (Entity) new Torpedo(ent.getInt("id"), new Position(ent.getJSONObject("position").getDouble("x"), ent.getJSONObject("position").getDouble("y")), new Team(ent.getJSONObject("owner").getString("name")), ent.getDouble("velocity"), new Direction(ent.getDouble("angle")));
+				entities.add(torpedo);
+			}
+		}
+		return entities;
+    }
 }
